e91f707d3972f13484650ab7bb0f3a8246691c05
Kosuke Kataoka <38752733+kataksk@users.noreply.github.com>
2021-12-10 18:50:20 +0900

    Add files via upload

--- order_gff_for_glimmer.py
+++ order_gff_for_glimmer.py
@@ -1,18 +1,39 @@
 import glob
+import sys
 
-tmp_gff_file_name = glob.glob('./tmp/*')
+gff_in = sys.argv[1]
+tmp_gff_file_name = [gff_in]
+# tmp_gff_file_name = glob.glob('./tmp/*')
 
+# Version 2021_12_07
+# for tmp_gff_file in tmp_gff_file_name:
+#     with open(tmp_gff_file, 'r') as f:
+#         tmp_list = list()
+#         for line in f:
+#             line_list = line.split('\t')
+#             if line_list[6] == '+':
+#                 print(line_list[0] + '\t' + line_list[3] + '\t' + line_list[4])
+#             elif line_list[6] == '-':
+#                 tmp_list.append([line_list[0], line_list[3], line_list[4]])
+#         if len(tmp_list) > 0:
+#             tmp_list.reverse()
+#             for tmp_list_line in tmp_list:
+#                 print(tmp_list_line[0], tmp_list_line[2], tmp_list_line[1])
+#         print()        
+
+# Version 2021_12_10
 for tmp_gff_file in tmp_gff_file_name:
     with open(tmp_gff_file, 'r') as f:
         tmp_list = list()
         for line in f:
             line_list = line.split('\t')
-            if line_list[6] == '+':
-                print(line_list[0] + '\t' + line_list[3] + '\t' + line_list[4])
-            elif line_list[6] == '-':
-                tmp_list.append([line_list[0], line_list[3], line_list[4]])
+            if line_list[2] == 'CDS':
+                if line_list[6] == '+':
+                    print(line_list[0] + '\t' + line_list[3] + '\t' + line_list[4])
+                elif line_list[6] == '-':
+                    tmp_list.append([line_list[0], line_list[3], line_list[4]])
         if len(tmp_list) > 0:
             tmp_list.reverse()
             for tmp_list_line in tmp_list:
                 print(tmp_list_line[0], tmp_list_line[2], tmp_list_line[1])
-        print()        
+        print()     
--- split_gff.py
+++ split_gff.py
@@ -8,23 +8,32 @@ gff_line_list = list()
 
 with open(gff_in, 'r') as f:
 
-    count = 0
+    line_count = 0
 
     for line in f:
         line_list = line.split('\t')
         gff_line_list.append(line_list)
-        count += 1
+        line_count += 1
 
         if line_list[2] == 'gene':
-            gene_line_list.append(count)
+            gene_line_list.append(line_count)
 
+    # print(gene_line_list)
+    # print(len(gene_line_list))
+    # for i in range(len(gene_line_list) - 1):
+    #     print(i)
+    
     count = 0
+
+    # print(gff_line_list[int(gene_line_list[0]) - 1 : int(gene_line_list[1]) - 1])
+    # print(gff_line_list[int(gene_line_list[940]) - 1 : int(gene_line_list[941]) - 1])
+    # print(gff_line_list[int(gene_line_list[941]) - 1 : int(gene_line_list[942]) - 1])
 
     for i in range(len(gene_line_list) - 1):
 
         tmp_gff = ''
 
-        for cds in gff_line_list[int(gene_line_list[i]) + 1 : int(gene_line_list[i+1]) - 1]:
+        for cds in gff_line_list[int(gene_line_list[i]) - 1 : int(gene_line_list[i+1]) - 1]:
             tmp_gff += '\t'.join(cds)
 
         with open('tmp/busco_gene_' + str(count) + '.gff', 'w') as f:
@@ -34,8 +43,16 @@ with open(gff_in, 'r') as f:
     
     tmp_gff = ''
 
-    for cds in gff_line_list[int(gene_line_list[len(gene_line_list) - 1]) + 1 :]:
+    # print(len(gene_line_list))
+    # 942
+    # print(gene_line_list[941])
+    # 22148
+    # print(gff_line_list[22148 - 1])
+    # ['scaffold_1146', 'AUGUSTUS', 'gene', '24711', '25194', '0.01', '+', '.', 'ID=169220at6656\n']
+
+    for cds in gff_line_list[gene_line_list[len(gene_line_list) - 1] - 1:]:
         tmp_gff += '\t'.join(cds)
+    # print(tmp_gff)
 
     with open('tmp/busco_gene_' + str(len(gene_line_list) - 1) + '.gff', 'w') as f:
         f.write(tmp_gff)
